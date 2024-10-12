6735073e5bb6380b4b29d93a282c7b37767fabd1
Atanu Mojumdar <atanu192003mojumdar@gmail.com>
2024-10-12 13:34:29 +0530

    MIME & headers

--- src/index.js
+++ src/index.js
@@ -20,17 +20,42 @@ requestEvent.on("request", (requestId) => {
     }
 });
 
+requestEvent.on("requeststatic", (requestId,requestURL) => {
+    const socket = SingleSocket;
+    if (socket) {
+        socket.emit("requeststatic", requestId,requestURL); 
+    }
+});
+
 app.get('/api', (req, res) => { //endpoint
     try{
+        console.log("route-1 request")
         const requestId = Date.now(); 
         pendingResponses[requestId] = res; 
     
         requestEvent.emit("request", requestId); 
     }
     catch(err){
-        res.status(500).send("Internal Server Error!")
+        console.log("route-1 request error")
+        return res.status(500).send("Internal Server Error!")
     }
 });
+
+app.get("/*",(req,res)=>{
+    try{
+        console.log("route-2 request")
+        const requestURL = req.url;
+        const requestId = Date.now(); 
+        pendingResponses[requestId] = res; 
+
+        requestEvent.emit("requeststatic", requestId,requestURL);
+    }
+    catch(err){
+        console.log("route-2 request error")
+        return res.status(500).send("Internal Server Error!");
+
+    }
+})
 
 io.use((socket, next) => {
     SingleSocket = socket;
@@ -40,13 +65,52 @@ io.use((socket, next) => {
 io.on("connection", (socket) => {
     console.log("New Socket Connected With ID:", socket.id);
 
-    socket.on("response", (data, requestId) => {
-        // console.log(data);
-        
-        if (pendingResponses[requestId]) {
-            const res = pendingResponses[requestId];
-            res.send(data); 
-            delete pendingResponses[requestId]; 
+    socket.on("response", (data,headers, requestId) => {
+        try{
+            // console.log(data);
+            
+            if (pendingResponses[requestId]) {
+                const res = pendingResponses[requestId];
+    
+                for (const key in headers) {
+                    if (Object.prototype.hasOwnProperty.call(headers, key)) {
+                        res.setHeader(key,headers[key]);
+                        
+                    }
+                }
+                console.log("route-1 response")
+                res.send(data); 
+                delete pendingResponses[requestId]; 
+            }
+        }
+        catch(err){
+            console.log("route-1 response error")
+            res.status(500).send("internal Server Error"); 
+        }
+    });
+
+    socket.on("responsestatic", (data,headers,requestId) => {
+        try{
+
+            // console.log(data,requestId,headers)
+            
+            if (pendingResponses[requestId]) {
+                const res = pendingResponses[requestId];
+    
+                for (const key in headers) {
+                    if (Object.prototype.hasOwnProperty.call(headers, key)) {
+                        res.setHeader(key,headers[key]);
+                        
+                    }
+                }
+                console.log("route-2 response")
+                res.send(data); 
+                delete pendingResponses[requestId]; 
+            }
+        }
+        catch(err){
+            console.log("route-2 response error")
+            res.status(500).send("internal Server Error"); 
         }
     });
 
